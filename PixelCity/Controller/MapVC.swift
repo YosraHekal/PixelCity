d7aab4b75f90d813ee9dcc37492a1bac829440dd
Minni K Ang <minni.ang@gmail.com>
2017-08-29 18:08:14 -0400

    mapView added

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -9,14 +9,23 @@
 import UIKit
 import Alamofire
 import AlamofireImage
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
     }
 
-
+    @IBAction func centerMapBtnPressed(_ sender: Any) {
+    }
+    
 
 }
 
+extension MapVC: MKMapViewDelegate {
+    
+}
