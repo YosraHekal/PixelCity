42e5e58d64bddccb525bb1a96b64c8542832c7a4
Minni K Ang <minni.ang@gmail.com>
2017-08-29 17:32:22 -0400

    installed dependencies

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -7,18 +7,15 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
 class MapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
