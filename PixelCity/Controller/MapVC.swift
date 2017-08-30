00df23547803b8920257dd27c006032c56176f43
Minni K Ang <minni.ang@gmail.com>
2017-08-30 12:22:42 -0400

    added photo subview and spinners

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -15,11 +15,18 @@ import CoreLocation
 class MapVC: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var revealView: UIView!
+    @IBOutlet weak var revealViewHeight: NSLayoutConstraint!
     
     var locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
+
     let regionRadius: Double = 1000
+    var screenSize = UIScreen.main.bounds
     
+    var spinner: UIActivityIndicatorView?
+    var progressLbl:UILabel?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
@@ -35,12 +42,41 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         mapView.addGestureRecognizer(doubleTap)
     }
     
-    func removePin() {
-        for annotation in mapView.annotations {
-            mapView.removeAnnotation(annotation)
-        }
+    func addSwipeDown() {
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(hideBottomView))
+        swipeDown.direction = .down
+        revealView.addGestureRecognizer(swipeDown)
     }
-
+    
+    func addSpinner() {
+        spinner  = UIActivityIndicatorView()
+        spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 150)
+        spinner?.activityIndicatorViewStyle = .whiteLarge
+        spinner?.color = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        spinner?.startAnimating()
+        revealView.addSubview(spinner!)
+    }
+    
+    func removeSpinner() {
+        spinner = UIActivityIndicatorView()
+        spinner!.removeFromSuperview()
+    }
+    
+    @objc func hideBottomView() {
+        showBottomView(show: false)
+    }
+    
+    func showBottomView(show: Bool) {
+        if show == true {
+            revealViewHeight.constant = 300
+        } else {
+            revealViewHeight.constant = 1
+        }
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+     }
+    
     @IBAction func centerMapBtnPressed(_ sender: Any) {
         if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
             centerMapOnUserLocation()
@@ -70,13 +106,23 @@ extension MapVC: MKMapViewDelegate {
     
     @objc func dropPin(sender: UITapGestureRecognizer) {
         removePin()
+        removeSpinner()
         let touchPoint = sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
         let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppedPin")
-        mapView.addAnnotation(annotation)
-        
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius * 2.0, regionRadius * 2.0)
+
         mapView.setRegion(coordinateRegion, animated: true)
+        mapView.addAnnotation(annotation)
+        self.showBottomView(show: true)
+        addSwipeDown()
+        addSpinner()
+    }
+    
+    func removePin() {
+        for annotation in mapView.annotations {
+            mapView.removeAnnotation(annotation)
+        }
     }
 }
 
