50e5865506803d1bc093d97f3994c4efa2f49ae5
Minni K Ang <minni.ang@gmail.com>
2017-09-01 15:01:04 -0400

    added 3dpeek&pop

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -48,6 +48,8 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         if collectionView != nil {
             revealView.addSubview(collectionView!)
         }
+        
+        registerForPreviewing(with: self, sourceView: collectionView!)
     }
     
     func addDoubleTap() {
@@ -246,5 +248,20 @@ extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource {
         { return }
         popVC.initData(forImage: imageArray[indexPath.row])
         present(popVC, animated: true)
+    }
+}
+
+extension MapVC: UIViewControllerPreviewingDelegate {
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        guard let indexPath = collectionView?.indexPathForItem(at: location), let cell = collectionView?.cellForItem(at: indexPath) else { return nil }
+        guard let popVC = storyboard?.instantiateViewController(withIdentifier: "PopVC") as? PopVC else { return nil }
+        
+        popVC.initData(forImage: imageArray[indexPath.row])
+        previewingContext.sourceRect = cell.contentView.frame
+        return popVC
+    }
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+        show(viewControllerToCommit, sender: self)
     }
 }
